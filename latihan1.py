97e7db2d2ec1f2f1172f283c7b59a614b39c6da4
mchoirule <mchoirul39@gmail.com>
2023-03-03 15:07:47 +0700

    tugas v1.1

--- latihan1.py
+++ latihan1.py
@@ -1,26 +1,13 @@
 #SOAL NO 1
+print("Soal No 1")
 for i in range(1,101):
     print(i)
 
 #SOAL NO 2
-print("Program bilangan bulat 1 hingga n")
-n = int(input("Masukkan bilangan bulat: "))
-for i in range(n+1):
+print("Soal No 2")
+for i in range(1,101):
   if(i%2 == 0):
     continue
   else:
     print(i)
-
-#SOAL NO 3
-print("Program bilangan bulat 1 hingga n")
-n = int(input("Masukkan bilangan bulat: "))
-def cek_genap_ganjil(bilangan):
-  for i in range(n):
-    if bilangan % 2 == 0:
-        print(f"{bilangan} adalah bilangan genap")
-    else:
-        print(f"{bilangan} adalah bilangan ganjil")
-
-for i in range(100):
-    cek_genap_ganjil(i)
 
